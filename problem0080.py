10abd04f65ab069fe94b591105eddf21d7eb9ef4
ChrisP Udacity <chris.pryby@udacity.com>
2015-03-12 11:44:39 -0400

    started p80

---
+++ problem0080.py
@@ -0,0 +1,54 @@
+# It is well known that if the square root of a natural number is not an
+# integer, then it is irrational. The decimal expansion of such square roots
+# is infinite without any repeating pattern at all.
+# 
+# The square root of two is 1.41421356237309504880..., and the digital sum of
+# the first one hundred decimal digits is 475.
+# 
+# For the first one hundred natural numbers, find the total of the digital sums
+# of the first one hundred decimal digits for all the irrational square roots.
+
+import time
+import math
+import numbertheory as nt
+import itertools as it
+import operator
+from fractions import gcd
+from copy import copy
+import re
+
+from math import floor,sqrt,log
+
+def prod(iterable):
+    return reduce(operator.mul, iterable, 1)
+
+factorial = [1,1,2,6,24,120,720,5040,40320,362880]
+
+t0 = time.clock()
+answer = None
+
+memo = {}
+
+def convergentOfSqrt2Times10ToThe198(k):
+    """Returns (a,b), where a/b is the kth convergent of sqrt(2), and a and b are coprime.
+The 1st iteration is 3/2."""
+    if k in memo:
+        return memo[k]
+    
+    if k == 1:
+        memo[1] = (3*(10**99),2)
+    else:
+        a,b = convergentOfSqrt2Times10ToThe198(k-1)
+        # add one, invert, add one
+        a += b
+        a,b = b,a
+        a += b
+        memo[k] = (a,b)
+
+    return memo[k]
+
+a,b = convergentOfSqrt2Times10ToThe198(2)
+print a,b,1.0*a/b
+
+print 'answer: {}'.format(answer) # 
+print 'seconds elapsed: {}'.format(time.clock()-t0) # ~
