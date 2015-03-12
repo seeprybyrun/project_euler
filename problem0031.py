b3a9c96eca5297113bf6fdc4b3f8869393c44a48
ChrisP Udacity <chris.pryby@udacity.com>
2015-03-12 02:08:37 -0400

    updated p31 a bit

--- problem0031.py
+++ problem0031.py
@@ -8,6 +8,11 @@
 # 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 # How many different ways can £2 be made using any number of coins?
 
+import time
+
+t0 = time.clock()
+answer = -1
+
 memo = {}
 
 def make_change(target,denoms):
@@ -28,9 +33,9 @@ def make_change(target,denoms):
     return memo[index]
 
 denoms = [1,2,5,10,20,50,100,200]
-target = 988
+target = 200
+answer = make_change(target,denoms)
 
-try:
-    print make_change(target,denoms)
-except Exception as e:
-    print e
+print 'answer: {}'.format(answer) # 73682
+print 'seconds elapsed: {}'.format(time.clock()-t0) # ~3.6ms
+
